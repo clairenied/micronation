b233fed0f6c280df79ac2c8e52b43f9f9d04ee41
clairernied <clairenied@gmail.com>
2017-01-20 14:23:20 -0600

    changing it back

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static(__dirname + '/public'))
+app.use('/', express.static('public'))
 
 microdb.sync({
   force: true
