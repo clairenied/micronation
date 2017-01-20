a8651f3501f2a1042eb4f8043cf28966ae34d04f
clairernied <clairenied@gmail.com>
2017-01-20 14:05:13 -0600

    file location chanege

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static('public'))
+app.use('/', express.static(__dirname + '/public'))
 
 microdb.sync({
   force: true
