32b88ce397fe28282b7152d02aa308d68e14f685
clairernied <clairenied@gmail.com>
2017-01-20 15:00:14 -0600

    fixing dirname

--- server/app.js
+++ server/app.js
@@ -24,8 +24,7 @@ app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static(__dirname +'/public'))
-console.log(__dirname +'/public')
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
 microDb.sync({
   force: true
